5a1f808a277323acf1256613958dfd1c9444b407
happycity0523 <sn2025126@shs.hs.kr>
2025-06-10 14:47:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("나의 첫 app")
 st.write("hello")
+name=st.text_inout("이름 입력")
+if name:
+   if name=="박서준"
+     st.success("반갑습니다. 박서준님!")
+else:
+     st.warning("누구세요?")
